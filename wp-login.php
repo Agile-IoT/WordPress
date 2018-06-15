ac488bcac1c924aeb00f4b13742e19e8b32cd11f
Eduard Brehm <eduard.brehm>
2018-06-15 14:55:17 +0200

    Check for user object is not NULL before accessing its method

--- wp-login.php
+++ wp-login.php
@@ -999,9 +999,9 @@ switch ( $action ) {
 				// If the user doesn't belong to a blog, send them to user admin. If the user can't edit posts, send them to their profile.
 				if ( is_multisite() && ! get_active_blog_for_user( $user->ID ) && ! is_super_admin( $user->ID ) ) {
 					$redirect_to = user_admin_url();
-				} elseif ( is_multisite() && ! $user->has_cap( 'read' ) ) {
+				} elseif ( is_multisite() && $user != NULL && ! $user->has_cap( 'read' ) ) {
 					$redirect_to = get_dashboard_url( $user->ID );
-				} elseif ( ! $user->has_cap( 'edit_posts' ) ) {
+				} elseif ( $user != NULL && ! $user->has_cap( 'edit_posts' ) ) {
 					$redirect_to = $user->has_cap( 'read' ) ? admin_url( 'profile.php' ) : home_url();
 				}
 
